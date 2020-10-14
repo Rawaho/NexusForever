8699639416345a2ec970ce4de7acaaa1b19dc069
Alex Anderson <alexanderson156@gmail.com>
2020-10-14 23:54:42 +0100

    Correctly Align ServerGroupInviteReceived
    Removed Unknown2 - this packet now works as expected and the group invite is displayed showing members of the group.
    
    Renamed Unknown1 & Unknown2 Based on @Kirmmin's screenshot in Discord.

--- Source/NexusForever.WorldServer/Network/Message/Model/ServerGroupInviteReceived.cs
+++ Source/NexusForever.WorldServer/Network/Message/Model/ServerGroupInviteReceived.cs
@@ -9,18 +9,16 @@ namespace NexusForever.WorldServer.Network.Message.Model
     public class ServerGroupInviteReceived : IWritable
     {
         public ulong GroupId { get; set; }
-        public uint Unknown0 { get; set; }
-        public uint Unknown1 { get; set; }
-        public uint Unknown2 { get; set; }
+        public uint LeaderIndex { get; set; }
+        public uint InviterIndex { get; set; }
 
         public List<GroupMember> Members = new List<GroupMember>();
 
         public void Write(GamePacketWriter writer)
         {
             writer.Write(GroupId);
-            writer.Write(Unknown0);
-            writer.Write(Unknown1);
-            writer.Write(Unknown2);
+            writer.Write(LeaderIndex);
+            writer.Write(InviterIndex);
 
             writer.Write(Members.Count);
             Members.ForEach(x => x.Write(writer));
